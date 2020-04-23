4c64e31d8754cfd588ce4e6652d7ac61152681fd
matt joseloff <msjo425@gmail.com>
2020-04-23 20:35:59 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,18 @@
+var recipes = {pasta:"water"}
+
+function updateObjectWithKeyAndValue(recipes,key,value){
+return Object.assign({},recipes,{[key]:value})
+return recipes
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+object[key] = value
+return object
+}
+
+function deleteFromObjectByKey(object, key){
+var newObj = Object.assign({},object)
+delete newObj.key
+return newObj
+return object
+}
